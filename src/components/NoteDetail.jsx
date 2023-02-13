bddfcedd856106b93671f9d13dd9b75b8aac8781
santihergon <santihergon19@gmail.com>
2023-02-13 20:14:48 +0100

    first commit

--- src/components/NoteDetail.jsx
+++ src/components/NoteDetail.jsx
@@ -1,10 +1,90 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
+import Typography from '@mui/material/Typography';
+import Grid from "@mui/material/Grid";
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { styled } from "@mui/material/styles";
+import TextareaAutosize from '@mui/base/TextareaAutosize';
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
-function NoteDetail() {
+const MiCard = styled(Grid)(({ theme }) => ({
+    color: "#f5f5f5",
+    // backgroundColor: db_base.color === "black" ? "#1A2027" : "#fff"
+}));
 
+function NoteDetail({ open, handleClose, modalInfo, deleteButton }) {
 
-
-    return <h1>hola</h1>
+    return (
+        <div>
+            {/* <Button onClick={handleOpen}>Open modal</Button> */}
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+            >
+                <Fade in={open}>
+                    <Grid item key={modalInfo.id} xs={12} sm={12} md={6} lg={6} xl={4} className='gridCards'>
+                        <MiCard container className="miCard" sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 600, m: 'auto', backgroundColor: modalInfo.color === "black" ? "#1A2027" : modalInfo.color === "green" ? "#2e9d50" : modalInfo.color === "yellow" ? "#c9b31b" : modalInfo.color === "blue" ? "#2a7bb5" : modalInfo.color === "white" ? "#919191" : "#8d5991" }} >
+                            <Grid item xs sx={{ p: '0.75em', '@media screen and (max-width: 890px)': { maxWidth: '100%', p: '0.75em' } }}>
+                                <div>
+                                        <TextField
+                                        id="outlined-multiline-flexible"
+                                        multiline
+                                        defaultValue={modalInfo.title}
+                                        fullWidth
+                                    />
+                                </div>
+                                <div className='pregunta'>
+                                    <TextField
+                                        id="outlined-multiline-flexible"
+                                        multiline
+                                        defaultValue={modalInfo.content}
+                                        fullWidth
+                                        autoFocus
+                                    />
+                                </div>
+                                <br></br>
+                                <div className='pregunta'>
+                                    <small>Created at: </small>
+                                    <span>{modalInfo.created_at}</span>.
+                                    {/* <span>{format(data.created_at, 'dd/mm/yyyy')}</span> */}
+                                </div>
+                                <IconButton aria-label="delete" onClick={() => deleteButton(modalInfo.id, modalInfo)}>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </Grid>
+                        </MiCard>
+                    </Grid>
+                    {/* <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2">
+                            {modalInfo.title}
+                        </Typography>
+                        <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box> */}
+                </Fade>
+            </Modal>
+        </div>
+    );
 }
 export default NoteDetail;
--- src/views/NoteList.jsx
+++ src/views/NoteList.jsx
@@ -1,12 +1,20 @@
+import { useState, useEffect } from "react";
+
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Masonry from "@mui/lab/Masonry";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+// import { format } from 'date-fns';
 
 import db_base from "../db/db_base.json";
 import Note from "../components/Note";
+import NoteDetail from "../components/NoteDetail";
+
 
 const heights = [
   350, 130, 190, 270, 310, 350, 330, 280, 250, 290, 200, 350, 230, 250, 280,
@@ -21,22 +29,66 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const MiCard = styled(Grid)(({ theme }) => ({
-  backgroundColor: "#3c3e44",
   color: "#f5f5f5",
+  // backgroundColor: db_base.color === "black" ? "#1A2027" : "#fff"
 }));
 
+const checkDb = () => {
+  console.log("aaa")
+  if (localStorage.getItem('db')) {
+    return JSON.parse(localStorage.getItem('db'))
+  }
+  localStorage.setItem("db", JSON.stringify(db_base))
+  return db_base
+}
+
 function NoteList() {
-  // const datos = JSON.parse(db_base);
-  console.log(db_base);
+  const [dataBase, setDataBase] = useState(checkDb());// Paso el json a un useState para que sea más accesible y rápido(?)
+  const [open, setOpen] = useState(false);
+  const [modalInfo, setModalInfo] = useState([]);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const updateDb = (newData) => {
+    localStorage.setItem("db", JSON.stringify(newData))
+    setDataBase(newData)
+  }
+
+  const deleteButton = (dataID, data) => {
+    console.log(dataID);
+
+    // const index1 = dataBase.indexOf(dataBase.find(nota => {
+    //   console.log("estamos en idnex 1 y recibo")
+    //   console.log(nota)
+    //   console.log(nota.id === dataID)
+    //   return nota.id === dataID
+    // }));
+    // console.log("index1 "+index1);    
+    // Este de abajo es lo mismo
+
+    const index = dataBase.indexOf(data); // Utilizo indexOf() para sacar el índice(posición en la lista) del objeto, en este caso el objeto es data, que es la nota 
+    console.log("index " + index);
+
+    dataBase.splice(index, 1);
+    updateDb([...dataBase]);
+
+    console.log(index);
+  }
 
   return (
     <section className="showcase">
       <Box sx={{ width: 1500, minHeight: 1393 }}>
         <Masonry columns={5} spacing={2}>
-          {db_base.map((data, key) => {
+          {dataBase.map((data, key) => {
             return (
               <Grid item key={data.id} xs={12} sm={12} md={6} lg={6} xl={4} className='gridCards'>
-                <MiCard container className="miCard" sx={{ m: 'auto' }}>
+                <MiCard container onClick={() => {
+                  // setOpen(true);
+                  handleOpen();
+                  setModalInfo(data);
+
+                }} className="miCard" sx={{ m: 'auto', backgroundColor: data.color === "black" ? "#1A2027" : data.color === "green" ? "#2e9d50" : data.color === "yellow" ? "#c9b31b" : data.color === "blue" ? "#2a7bb5" : data.color === "white" ? "#919191" : "#8d5991" }} >
                   <Grid item xs sx={{ p: '0.75em', '@media screen and (max-width: 890px)': { maxWidth: '100%', p: '0.75em' } }}>
                     <div>
                       <Typography variant="h5" component="div" className="Typography">
@@ -48,9 +100,13 @@ function NoteList() {
                     </div>
                     <br></br>
                     <div className='pregunta'>
-                      <small>Created at:</small>
-                      <span>{data.created_at}</span>
+                      <small>Created at: </small>
+                      <span>{data.created_at}</span>.
+                      {/* <span>{format(data.created_at, 'dd/mm/yyyy')}</span> */}
                     </div>
+                    <IconButton aria-label="delete" onClick={() => deleteButton(data.id, data)}>
+                      <DeleteIcon />
+                    </IconButton>
                   </Grid>
                 </MiCard>
               </Grid>
@@ -61,6 +117,7 @@ function NoteList() {
             ))} */}
         </Masonry>
       </Box>
+      <NoteDetail open={open} handleClose={handleClose} modalInfo={modalInfo} deleteButton={deleteButton} />
     </section>
   );
 }
